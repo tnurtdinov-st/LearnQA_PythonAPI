0b41a3b65a8c0bdbe80d64053ac417d02b2f9f85
tnurt <blf232@gmail.com>
2022-12-11 16:20:39 +0300

    compare_query_type

--- compare_query_type.py
+++ compare_query_type.py
@@ -20,7 +20,7 @@ print("3: " + response.text)
 params = ("GET", "POST", "PUT", "DELETE")
 for i in params:
     response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params={"method": i})
-    print("4GET " + i+ ": " + response.text)
+    print("4 GET - " + i+ ": " + response.text)
     if "GET" not in i:
         if "success" in response.text:
             print("warning!!! incorrect response for GET and " + i)
@@ -28,21 +28,21 @@ for i in params:
 
 for i in params:
     response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": i})
-    print("4POST " + i+ ": " + response.text)
+    print("4 POST - " + i+ ": " + response.text)
     if "POST" not in i:
         if "success" in response.text:
             print("warning!!! incorrect response for POST and " + i)
 
 for i in params:
     response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": i})
-    print("4PUT " + i+ ": " + response.text)
+    print("4 PUT - " + i+ ": " + response.text)
     if "PUT" not in i:
         if "success" in response.text:
             print("warning!!! incorrect response for PUT and " + i)
 
 for i in params:
     response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data={"method": i})
-    print("4DELETE " + i+ ": " + response.text)
+    print("4 DELETE - " + i+ ": " + response.text)
     if "DELETE" not in i:
         if "success" in response.text:
             print("warning!!! incorrect response for DELETE and " + i)
