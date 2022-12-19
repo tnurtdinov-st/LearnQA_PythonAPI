59cb1117ca00c0ebaf2b8704b3aa2c2c5788b1dd
tnurtdinov <blf232@gmail.com>
2022-12-19 17:42:13 +0300

    Restructure

--- tests/test_user_auth.py
+++ tests/test_user_auth.py
@@ -1,23 +1,24 @@
 import requests
 import pytest
+from lib.base_case import BaseCase
 
-class TestUserAuth:
+class TestUserAuth(BaseCase):
 
     exclude_params = [("no_cookie"), ("no_token") ]
-    
+
     def setup(self):
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
         }
         response1 = requests.post("https://playground.learnqa.ru/api/user/login", data=data, verify=False)
-        print(response1.headers)
-        assert "auth_sid" in response1.cookies, "There is no auth cookie in the response"
-        assert "x-csrf-token" in response1.headers, "There is no CSRF token in the response"
+
+        self.auth_sid = self.get_cookie(response1, "auth_sid")
+        self.token = self.get_header(response1, "x-csrf-token")
+        self.user_id_auth = self.get_json_value(response1, "user_id")
+
         assert "user_id" in response1.json(), "There is no user id in the response"
         print(response1.json())
-        self.auth_sid = response1.cookies.get("auth_sid")
-        self.token = response1.headers.get("x-csrf-token")
         self.user_id_auth = response1.json()["user_id"]
 
     def test_auth_user(self):
