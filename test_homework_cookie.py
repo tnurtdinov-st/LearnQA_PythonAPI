3d0a06db6926e60b7d5cfc74d81d978260e9a424
tnurtdinov <blf232@gmail.com>
2022-12-19 14:18:32 +0300

    test_homework_cookie

--- test_homework_cookie.py
+++ test_homework_cookie.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 class TestFirstApi:
-    def test_hello_call(self):
+    def test_homework_cookie(self):
         url = "https://playground.learnqa.ru/ajax/api/homework_cookie"
         response = requests.get(url, verify=False)
         print(response.text)
