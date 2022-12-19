1d4a4b85fee21c636d1b88b840642d67ddd5d2d6
tnurtdinov <blf232@gmail.com>
2022-12-19 12:58:38 +0300

    longtime_job fix

--- longtime_job.py
+++ longtime_job.py
@@ -1,15 +1,16 @@
 import requests
 import time
 
-response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
+response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", verify=False)
 token = response.json()['token']
-print("Token: " + token)
+t = response.json()['seconds']
+print("Token: " + token + " Time: " + str(t))
 
 while True:
-    response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": token})
+    response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": token}, verify=False)
     if "Job is NOT ready" in response.json()['status']:
         print("Status: " + response.json()['status'])
-        time.sleep(20)
+        time.sleep(int(t))
     elif "Job is ready" in response.json()['status']:
         print("Status: " + response.json()['status'])
         print("Result: " + response.json()['result'])
