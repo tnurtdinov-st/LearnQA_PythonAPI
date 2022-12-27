d5991f070f9fc88e7e607a604e7fdaa48822dd45
tnurtdinov <blf232@gmail.com>
2022-12-27 15:30:46 +0300

    test_user_register.py

---
+++ tests/test_user_register.py
@@ -0,0 +1,128 @@
+import requests
+import pytest
+import string
+import random
+from lib.base_case import BaseCase
+from lib.assertions import Assertions
+from datetime import datetime
+
+class TestUserRegister(BaseCase):
+
+
+    def setup(self):
+        base_part = "learnqa"
+        domain = "example.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        self.email = f"{base_part}{random_part}@{domain}"
+        self.incorrect_email = f"{base_part}{random_part}{domain}"
+
+
+    def test_create_user_succsessfuly(self):
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.asset_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
+
+
+    def test_create_user_with_existing_email(self):
+        email = 'vinkotov@example.com'
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': email
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/", data = data)
+
+        Assertions.asset_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+    def test_incorrect_email(self):
+        data = {
+            'password': '123',
+            'username': 'learnqa',
+            'firstName': 'learnqa',
+            'lastName': 'learnqa',
+            'email': self.incorrect_email
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        print(response.text)
+        Assertions.asset_code_status(response, 400)
+        assert response.content.decode("utf-8") == f"Invalid email format", f"Unexpected response content {response.content}"
+
+    random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+    data = [
+        {'username': 'lernqa',
+         'firstName': 'lernqa',
+         'lastName': 'lernqa',
+         'email': f"lernqa{random_part}@example.com"},
+        {'password': '123',
+         'firstName': 'lernqa',
+         'lastName': 'lernqa',
+         'email': f"lernqa{random_part}@example.com"},
+        {'password': '123',
+         'username': 'lernqa',
+         'lastName': 'lernqa',
+         'email': f"lernqa{random_part}@example.com"},
+        {'password': '123',
+         'username': 'lernqa',
+         'firstName': 'lernqa',
+         'email': f"lernqa{random_part}@example.com"},
+        {'password': '123',
+         'username': 'lernqa',
+         'firstName': 'lernqa',
+         'lastName': 'lernqa'}
+    ]
+
+    @pytest.mark.parametrize('data', data)
+    def test_create_without_filed(self, data):
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        Assertions.asset_code_status(response, 400)
+        if "password" not in data:
+            assert response.content.decode("UTF-8") == "The following required params are missed: password"
+        elif "username" not in data:
+            assert response.content.decode("UTF-8") == "The following required params are missed: username"
+        elif "firstName" not in data:
+            assert response.content.decode("UTF-8") == "The following required params are missed: firstName"
+        elif "lastName" not in data:
+            assert response.content.decode("UTF-8") == "The following required params are missed: lastName"
+        elif "email" not in data:
+            assert response.content.decode("UTF-8") == "The following required params are missed: email"
+
+    def test_create_user_with_short_name(self):
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        data = {
+            'password': '123',
+            'username': 'l',
+            'firstName': 'lernqa',
+            'lastName': 'lernqa',
+            'email': f"lernqa{random_part}@example.com"
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        Assertions.asset_code_status(response, 400)
+        assert response.content.decode("UTF-8") == "The value of 'username' field is too short"
+
+    def test_create_user_with_long_name(self):
+        letters = string.ascii_lowercase
+        name = ''.join(random.choice(letters) for i in range(251))
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        data = {
+            'password': '123',
+            'username': name,
+            'firstName': 'lernqa',
+            'lastName': 'lernqa',
+            'email': f"lernqa{random_part}@example.com"
+        }
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+        Assertions.asset_code_status(response, 400)
+        assert response.content.decode("UTF-8") == "The value of 'username' field is too long"
